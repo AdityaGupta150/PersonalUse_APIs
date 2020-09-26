d7952af627fd1a6623f04ce9f7bf67aaee6a4317
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2020-09-26 21:32:53 +0530

    Merge from Development branch (#5)
    * Boylestad
    
    * Somehtin
    
    * /edu route fixed

--- apis/eduPurpose/app.js
+++ apis/eduPurpose/app.js
@@ -1,20 +1,61 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-const mongoose = require('mongoose');
+const cricapi = require('cricapi')
+cricapi.setAPIKey("dmkLt572pHdnImPZpfgWdQbuaL72");
+
+//---------------------------------------------------
+const {Schema} = require('mongoose');
 const { getConnection } = require('../util/mongoConnection');
 
-const edu = getConnection('edu').model('edu', new mongoose.Schema({
-    name: String,
-    pass: String,
+const edu = getConnection('edu').model('edu', new Schema({
+    mail: String,
+    comment: String,
 }));
 
-function saveToDB(data) {
-    edu.create({name: data.name, pass: data.pass})
+function saveComment(data) {
+    edu.create({mail: data.mail_id, comment: data.comm})
         .catch(err => console.log(err));
-
 }
 
+app.get('/data', (req, res) => {
+    edu.find({}, (err, docs) => {
+       if(err)  console.log(`Error: ` + err)
+        else res.send(docs)
+    });
+})
+
+app.get('/cric1', (req, res) => {
+    const obj = cricapi.cricketMatches( //current matches
+        (data,data2,data3) => {
+            console.log(data);
+            console.log(data2);
+            console.log(data3);
+            res.send({
+                'd1': data,
+                'd2': data2,
+                'd3': data3
+            });
+        })
+
+    });
+
+app.get('/cric2', (req, res) => {
+    const obj = cricapi.matches(
+        (data,data2,data3) => {
+            console.log(data);
+            console.log(data2);
+            console.log(data3);
+            res.send({
+                'd1': data,
+                'd2': data2,
+                'd3': data3
+            });
+        }
+    );
+})
+
+//---------------------------------------------------
 app.get('/', (req,res) => res.send('Welcome to Edu'))
 
 app.get('/bhuvnesh', (req,res) => {
@@ -32,11 +73,14 @@ app.post('/submitIt', (req, res) => {
     })
 })
 
-app.get('/data', (req, res) => {
-    edu.find({}, (err, docs) => {
-       if(err)  console.log(`Error: ` + err)
-        else res.send(docs)
-    });
+app.post('/comment', (req, res) => {
+    saveComment(req.body)
+    res.json({
+        'You sent this:' : {
+            'email': req.body.mail_id,
+            'comment': req.body.comm,
+        }
+    })
 })
 
 module.exports = app;
