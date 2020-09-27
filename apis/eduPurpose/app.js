3c5416b81d364597939679859e22bee08cbae568
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2020-09-27 16:25:51 +0530

    Update app.js

--- apis/eduPurpose/app.js
+++ apis/eduPurpose/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const cricapi = require('cricapi')
-cricapi.setAPIKey("dmkLt572pHdnImPZpfgWdQbuaL72");
 
 //---------------------------------------------------
 const {Schema} = require('mongoose');
